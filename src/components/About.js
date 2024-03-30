7d5796a6395aef349a0de63935ba72fd4d455686
zainab mirza <zainabmirzafatah99@gmail.com>
2024-03-30 23:51:59 +0300

    added

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">About
+  <h2>About Me</h2>
+  <p>Zainab's World!</p>
+  <img src={image} alt="I made this" />
+  </div>);
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (<nav>NavBar
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>);
 }
 
 export default NavBar;
